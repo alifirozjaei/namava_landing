b0e837d851d938fae826951471ca44cf0a747939
Ali L Firozjaeai <ali.firozjaeii@gmail.com>
2023-09-02 12:01:51 +0330

    Add Navbar
    This commit introduces the addition of a navbar component to the project. The navbar serves as a navigation menu for easy access to different sections or pages within the project. This commit includes the necessary HTML, CSS, and JS files for the navbar, ensuring its proper functionality and visual styling.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
-window.addEventListener('load', function() {
-  console.log('main.js loaded');
-})
+window.addEventListener("load", function () {
+  console.log("main.js loaded");
+
+  // when navbar scroll down hide it. when scroll up show it.
+  let prevScrollpos = window.pageYOffset;
+  this.window.addEventListener("scroll", () => {
+    let currentScrollPos = window.pageYOffset;
+    let navbar = document.getElementById("navbar");
+
+    if (prevScrollpos > currentScrollPos) {
+      // if scroll down hide navbar
+      navbar.style.top = "0";
+    } else {
+      // if scroll up show navbar
+      navbar.style.top = "-100px";
+    }
+
+    if (currentScrollPos == 0) {
+      navbar.style.background = "transparent";
+    } else {
+      navbar.style.background = "#000";
+    }
+
+    prevScrollpos = currentScrollPos;
+  });
+
+
+  
+});
