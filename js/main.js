95343bfc0ff31ae14ceedb6a7a837bfc5b34f890
Ali L Firozjaeai <ali.firozjaeii@gmail.com>
2023-09-13 23:25:39 +0330

    Add FAQ Section

--- js/main.js
+++ js/main.js
@@ -24,8 +24,41 @@ window.addEventListener("load", function () {
     prevScrollpos = currentScrollPos;
   });
 
+  // expand config section
+  document
+    .getElementById("config-expand-button")
+    .addEventListener("click", () => {
+      document.getElementById("config-box").classList.toggle("config-expand");
+    });
 
-  document.getElementById("config-expand-button").addEventListener('click',() => {
-    document.getElementById("config-box").classList.toggle("config-expand"); 
-  })
+  // collapsible question
+  let coll = document.getElementsByClassName("collapsible");
+  let prevActiveCollapsibleItem = null;
+  let prevActiveCollapsibleItemContent = null;
+  for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+      // close prev item
+      if (
+        prevActiveCollapsibleItem != null &&
+        prevActiveCollapsibleItem != this
+      ) {
+        prevActiveCollapsibleItem.classList.remove("active");
+        prevActiveCollapsibleItemContent.style.maxHeight = null;
+      }
+
+      // toggle current item
+      this.classList.toggle("active");
+      let content = this.nextElementSibling;
+      if (content.style.maxHeight) {
+        // close
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+
+      // save current item as prev item
+      prevActiveCollapsibleItem = this;
+      prevActiveCollapsibleItemContent = content;
+    });
+  }
 });
